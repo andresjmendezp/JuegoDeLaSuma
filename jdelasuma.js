dc0b1f05d5bcd62ded8af14f5ad1d1b76e8d3e08
Andres Mendez <andresjmendezp@gmail.com>
2020-07-29 00:25:39 -0500

    Se ingreso las imagenes del 1 y 9 pero no las dibuja

--- jdelasuma.js
+++ jdelasuma.js
@@ -1,14 +1,34 @@
+var imagenes = [];
+imagenes ["1"]="uno.png"
+imagenes ["9"]="nueve.png"
+
+class Numero
+{
+  constructor(v)
+  {
+    this.valor=v;
+    this.imagen = new Image();
+
+    this.imagen.src =imagenes[this.valor];
+  }
+}
+
 var vp= document.getElementById("Tablero");
 var papel= vp.getContext("2d");
 
 
 dibujarLinea("blue",0,0,1200,0,papel);
-dibujarLinea("blue",1200,0,1200,500,papel);
-dibujarLinea("blue",1200,500,0,500,papel);
-dibujarLinea("blue",0,500,0,0,papel);
+dibujarLinea("blue",1200,0,1200,700,papel);
+dibujarLinea("blue",1200,700,0,700,papel);
+dibujarLinea("blue",0,700,0,0,papel);
 
+var numeros=[];
+numeros.push(new Numero(1));
+numeros.push(new Numero(9));
 
-
+console.log("Antes de cargar la imagen");
+papel.drawImage(numeros[0].imagen,0,0);
+console.log("Despues de cargar la imagen");
 
 function dibujarLinea(color,xinicial,yinicial,xfinal,yfinal,lienzo)
 {
@@ -23,3 +43,11 @@ function dibujarLinea(color,xinicial,yinicial,xfinal,yfinal,lienzo)
 
 
 }
+
+function aleatorio(min,maxi)
+{
+  var resultado;
+  resultado= Math.floor(Math.random()*(maxi-min+1))+min;
+  return resultado;
+}
+
