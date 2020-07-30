575e59c9f1ba2434aff9a404dd5fa4f3f1cd1cae
Andres Mendez <andresjmendezp@gmail.com>
2020-07-29 23:26:17 -0500

    Se completaron los numeros hasta el 9, incluyo el mas y se cuadro el container y las fuentes, para ingresar la respuesta

--- jdelasuma.js
+++ jdelasuma.js
@@ -2,10 +2,15 @@
 var vp= document.getElementById("Tablero");
 var papel= vp.getContext("2d");
 
-dibujarLinea("blue",0,0,1200,0,papel);
-dibujarLinea("blue",1200,0,1200,700,papel);
-dibujarLinea("blue",1200,700,0,700,papel);
-dibujarLinea("blue",0,700,0,0,papel);
+dibujarLinea("blue",0,0,1300,0,papel);
+dibujarLinea("blue",1300,0,1300,600,papel);
+dibujarLinea("blue",1300,600,0,600,papel);
+dibujarLinea("blue",0,600,0,0,papel);
+
+var mas={
+  url:"mas.png",
+  cargaOk: false
+}
 
 var uno={
     url:"uno.png",
@@ -20,13 +25,42 @@ var tres={
   cargaOk: false
 }
 
+var cuatro={
+  url:"cuatro.png",
+  cargaOk: false
+}
+
+var cinco={
+  url:"cinco.png",
+  cargaOk: false
+}
+
+var seis={
+  url:"seis.png",
+  cargaOk: false
+}
+
+var siete={
+  url:"siete.png",
+  cargaOk: false
+}
+
+var ocho={
+  url:"ocho.png",
+  cargaOk: false
+}
+
 var nueve={
   url:"nueve.png",
   cargaOk: false
 }
 
-x=aleatorio(1,3);
-y=aleatorio(1,3);
+x=aleatorio(1,9);
+y=aleatorio(1,9);
+
+mas.imagen=new Image();
+mas.imagen.src=mas.url;
+mas.cargaOk=true;
 
 uno.imagen=new Image();
 uno.imagen.src=uno.url;
@@ -37,8 +71,24 @@ dos.imagen.src=dos.url;
 tres.imagen=new Image();
 tres.imagen.src=tres.url;
 
+cuatro.imagen=new Image();
+cuatro.imagen.src=cuatro.url;
+
+cinco.imagen=new Image();
+cinco.imagen.src=cinco.url;
+
+seis.imagen=new Image();
+seis.imagen.src=seis.url;
+
+siete.imagen=new Image();
+siete.imagen.src=siete.url;
+
+ocho.imagen=new Image();
+ocho.imagen.src=ocho.url;
+
 nueve.imagen=new Image();
 nueve.imagen.src=nueve.url;
+
 
 if(x==1){
 uno.imagen.addEventListener("load",cargaruno);
@@ -49,34 +99,155 @@ if(x==2){
 if(x==3){
   tres.imagen.addEventListener("load",cargartres);
 }
+if(x==4){
+  cuatro.imagen.addEventListener("load",cargarcuatro);
+}
+if(x==5){
+  cinco.imagen.addEventListener("load",cargarcinco);
+}
+if(x==6){
+  seis.imagen.addEventListener("load",cargarseis);
+}
+if(x==7){
+  siete.imagen.addEventListener("load",cargarsiete);
+}
+if(x==8){
+  ocho.imagen.addEventListener("load",cargarocho);
+}
+if(x==9){
+  nueve.imagen.addEventListener("load",cargarnueve);
+}
 
+if(y==1){
+  uno.imagen.addEventListener("load",cargaruno1);
+  }
+if(y==2){
+    dos.imagen.addEventListener("load",cargardos1);
+  }
+if(y==3){
+    tres.imagen.addEventListener("load",cargartres1);
+  }
+if(y==4){
+    cuatro.imagen.addEventListener("load",cargarcuatro1);
+  }
+if(y==5){
+    cinco.imagen.addEventListener("load",cargarcinco1);
+  }
+if(y==6){
+    seis.imagen.addEventListener("load",cargarseis1);
+  }
+  if(y==7){
+    siete.imagen.addEventListener("load",cargarsiete1);
+  }
+  if(y==8){
+    ocho.imagen.addEventListener("load",cargarocho1);
+  }
+  if(y==9){
+    nueve.imagen.addEventListener("load",cargarnueve1);
+  }
 
-nueve.imagen.addEventListener("load",cargarnueve);
-
+mas.imagen.addEventListener("load",dibujarmas);
 
 function cargaruno()
 {
   uno.cargaOk =true;
   dibujar1(uno);
 }
+function cargaruno1()
+{
+  uno.cargaOk =true;
+  dibujar2(uno);
+}
+
+
+
 function cargardos()
 {
   dos.cargaOk =true;
   dibujar1(dos);
 }
+function cargardos1()
+{
+  dos.cargaOk =true;
+  dibujar2(dos);
+}
+
+
 function cargartres()
 {
   tres.cargaOk =true;
   dibujar1(tres);
 }
+function cargartres1()
+{
+  tres.cargaOk =true;
+  dibujar2(tres);
+}
 
+function cargarcuatro()
+{
+  cuatro.cargaOk =true;
+  dibujar1(cuatro);
+}
+function cargarcuatro1()
+{
+  cuatro.cargaOk =true;
+  dibujar2(cuatro);
+}
 
+function cargarcinco()
+{
+  cinco.cargaOk =true;
+  dibujar1(cinco);
+}
+function cargarcinco1()
+{
+  cinco.cargaOk =true;
+  dibujar2(cinco);
+}
 
+function cargarseis()
+{
+  seis.cargaOk =true;
+  dibujar1(seis);
+}
+function cargarseis1()
+{
+  seis.cargaOk =true;
+  dibujar2(seis);
+}
+
+function cargarsiete()
+{
+  siete.cargaOk =true;
+  dibujar1(siete);
+}
+function cargarsiete1()
+{
+  siete.cargaOk =true;
+  dibujar2(siete);
+}
+
+function cargarocho()
+{
+  ocho.cargaOk =true;
+  dibujar1(ocho);
+}
+function cargarocho1()
+{
+  ocho.cargaOk =true;
+  dibujar2(ocho);
+}
 
 function cargarnueve()
 {
   nueve.cargaOk =true;
-  dibujar2();
+  dibujar1(nueve);
+}
+function cargarnueve1()
+{
+  nueve.cargaOk =true;
+  dibujar2(nueve);
 }
 
 
@@ -84,19 +255,24 @@ function cargarnueve()
 function dibujar1(n){
     if (n.cargaOk)
     {
-    console.log("antes de dibujar");
-    papel.drawImage(n.imagen,50,120);
-    console.log("despues de dibujar");
+   ;
+    papel.drawImage(n.imagen,20,120);
+    
     }
 }
-function dibujar2(){
-  if (nueve.cargaOk)
+function dibujar2(n){
+  if (n.cargaOk)
   {
-  console.log("antes de dibujar");
-  papel.drawImage(nueve.imagen,550,120);
-  console.log("despues de dibujar");
+    papel.drawImage(n.imagen,740,120);
   }
 }
+
+function dibujarmas()
+{
+  console.log("se dibuja mas")    
+  papel.drawImage(mas.imagen,520,220);
+}
+
 
 function dibujarLinea(color,xinicial,yinicial,xfinal,yfinal,lienzo)
 {
